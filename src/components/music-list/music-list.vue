9be53181a360d2d9e1e2ff98a31a4a2a6cb1dcbf
KEWEN <kewen@huazhu.com>
2018-09-14 16:43:05 +0800

    '6-13'

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -20,8 +20,11 @@
 <script>
 import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
+import {prefixStyle} from 'common/js/dom'
 
 const RESERVED_HEIGHT = 40
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
 
 export default {
   name: '',
@@ -64,8 +67,7 @@ export default {
       let zIndex = 0
       let scale = 1 // 图片放大比例
       let blur = 0
-      this.$refs.layer.style['transform'] = `translate3d(0, ${translateY}px, 0)`
-      this.$refs.layer.style['webkitTransform'] = `translate3d(0, ${translateY}px, 0)`
+      this.$refs.layer.style[transform] = `translate3d(0, ${translateY}px, 0)`
       
       // 向下拉时
       const percent = Math.abs(newY / this.imageHeight)
@@ -75,8 +77,7 @@ export default {
       }else{
         blur = Math.min(20 * percent, 20)
       }
-      this.$refs.filter.style['backdrop-filter'] = `blur(${blur}px)`
-      this.$refs.filter.style['webkitBackdrop-filter'] = `blur(${blur}px)`
+      this.$refs.filter.style[backdrop] = `blur(${blur}px)`
       if(newY<this.minTranslateY) {
         zIndex = 10
         this.$refs.bgImage.style.paddingTop = 0
@@ -86,8 +87,7 @@ export default {
         this.$refs.bgImage.style.height = 0
       }
       this.$refs.bgImage.style.zIndex = zIndex      
-      this.$refs.bgImage.style['transform'] = `scale(${scale})`
-      this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`
+      this.$refs.bgImage.style[transform] = `scale(${scale})`
     }
   },
 
